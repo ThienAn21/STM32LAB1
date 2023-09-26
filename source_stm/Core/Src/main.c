6e52ed25fbfe871c4a0741c26d7d8ecaa634053d
ThienAn03 <hvt91vuhoangthienan@gmail.com>
2023-09-27 01:24:33 +0700

    Init for ex5

--- source_stm/Core/Src/main.c
+++ source_stm/Core/Src/main.c
@@ -146,16 +146,39 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_A1_Pin|LED_B1_Pin|LED_C1_Pin|LED_D1_Pin
+                          |LED_E1_Pin|LED_F1_Pin|LED_G1_Pin|LED_RED1_Pin
+                          |LED_GREEN1_Pin|LED_YELLOW1_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : LED_RED_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LED_A2_Pin|LED_B2_Pin|LED_C2_Pin|LED_D2_Pin
+                          |LED_E2_Pin|LED_F2_Pin|LED_G2_Pin|LED_RED2_Pin
+                          |LED_GREEN2_Pin|LED_YELLOW2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_A1_Pin LED_B1_Pin LED_C1_Pin LED_D1_Pin
+                           LED_E1_Pin LED_F1_Pin LED_G1_Pin LED_RED1_Pin
+                           LED_GREEN1_Pin LED_YELLOW1_Pin */
+  GPIO_InitStruct.Pin = LED_A1_Pin|LED_B1_Pin|LED_C1_Pin|LED_D1_Pin
+                          |LED_E1_Pin|LED_F1_Pin|LED_G1_Pin|LED_RED1_Pin
+                          |LED_GREEN1_Pin|LED_YELLOW1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_RED_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED_A2_Pin LED_B2_Pin LED_C2_Pin LED_D2_Pin
+                           LED_E2_Pin LED_F2_Pin LED_G2_Pin LED_RED2_Pin
+                           LED_GREEN2_Pin LED_YELLOW2_Pin */
+  GPIO_InitStruct.Pin = LED_A2_Pin|LED_B2_Pin|LED_C2_Pin|LED_D2_Pin
+                          |LED_E2_Pin|LED_F2_Pin|LED_G2_Pin|LED_RED2_Pin
+                          |LED_GREEN2_Pin|LED_YELLOW2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
